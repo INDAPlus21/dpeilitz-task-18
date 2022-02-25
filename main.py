8b271d967f4efca0612958586893fc4d73a844ec
dpeilitz <dpeilitz@kth.se>
2022-02-25 14:52:46 +0100

    Comments and cleanup

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import json
 
-
 word_dict = {}
 
 
 def write_to_Dict():
+    #Goes through the the token.txt file and enters every line into the word_dict dictionary. Skips every duplicate letter.
     with open('token.txt', 'r') as token:
         lines = token.readlines()
         #temp = 0
@@ -23,20 +23,22 @@ def write_to_Dict():
 
 
 def write_indexFile():
+    #Prints the content of the word_dict to the indexFile
+
     with open('indexFile.txt', "w") as file:
         output = ""
         for data in word_dict.keys():
-            print(data)
             output += data
             for i in word_dict[data]:
                 output += " %s " % i
             print(output)
             file.write(output + "\n")
             output = ""
-            # index.write(data[0] + " " + data[1] + "\n")
+
 
 
 def find_word(word):
+    #Scans through the corpus, finds all occurences of the word and prints the sentence that they are in.
     get_magic()
     print(word_dict["aa"])
     value = word_dict[word]
@@ -49,26 +51,31 @@ def find_word(word):
 
 
 def write_magicFile():
+    # Writes the word_dict to the magicFile and stores it as a json.
     magic = open("magicFile.json", "w")
     json.dump(word_dict, magic)
     magic.close()
 
 
 def get_magic():
+    # Gets all data from the magic file and outputs it to the word_dict 
     with open('magicFile.json') as file:
         global word_dict
         word_dict = json.load(file)
 
 
 def test():
+    #REMOVE ASAP
     search = input("Slå in sökord vettja")
     find_word(search)
 
 
 def construct():
+    #Constructs everything
     write_to_Dict()
     write_indexFile()
     write_magicFile()
 
+#construct()
 
 find_word("a")
